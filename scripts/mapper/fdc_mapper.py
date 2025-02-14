738d72364453694e9de6a46b4a918bd2f64e0fa1
Estrella <estrella.paoli1@gmail.com>
2025-02-13 21:53:17 -0500

    removed unused variables

--- scripts/mapper/fdc_mapper.py
+++ scripts/mapper/fdc_mapper.py
@@ -9,14 +9,12 @@ WANTED_COUNTRY = "Canada"
 
 def map_fdc_dict_to_product(product_dict: dict, categories):
     """Maps a fdc dictionary to a product object"""
-    country_field = 'marketCountry'
     id_field = 'gtinUpc'
     product_name_field = 'description'
     generic_name_field = 'description'
     brands_field = 'brandName'
     brand_owner_field = 'brandOwner'
     food_groups_en_field = 'brandedFoodCategory'  # TODO convert fdc categories to off food groups
-    allergens_en_field = ''
 
     if product_dict['brandedFoodCategory'] in categories.keys():
         categories[product_dict['brandedFoodCategory']] += 1
@@ -31,7 +29,8 @@ def map_fdc_dict_to_product(product_dict: dict, categories):
         product_name=product_dict[product_name_field].title(),
         generic_name_en=product_dict[generic_name_field].title(),
         is_raw=fdc_is_raw_aliment(product_dict['brandedFoodCategory']),
-        brands=[product_dict[brand_owner_field].title(), product_dict[brands_field].title()] if brands_field in product_dict.keys() else [product_dict[brand_owner_field].title()],
+        brands=[product_dict[brand_owner_field].title(), product_dict[brands_field].title()] if brands_field in product_dict.keys() else [
+            product_dict[brand_owner_field].title()],
         brand_owner=product_dict[brand_owner_field].title(),
         food_groups_en=product_dict[food_groups_en_field].split(','),
         ingredients=map_fdc_dict_to_ingredients(product_dict['ingredients']),
@@ -49,9 +48,8 @@ def fdc_is_raw_aliment(category: str):
     if category in ['Vegetables  Unprepared/Unprocessed (Frozen)', 'Fruits, Vegetables & Produce', 'Vegetables - Unprepared/Unprocessed (Frozen)']:
         is_raw = True
     elif category == 'Pre-Packaged Fruit & Vegetables':
-        is_raw = None #currently no way of knowing if the product is raw, there should be a more complex analysis
+        is_raw = None  # currently no way of knowing if the product is raw, there should be a more complex analysis
     return is_raw
-
 
 
 def map_fdc_dict_to_ingredients(ingredients: str) -> Ingredients:
@@ -76,7 +74,7 @@ def map_fdc_dict_to_nutrition_facts(food_nutrients: list[dict]) -> NutritionFact
     sugar_level = next((item['amount'] for item in food_nutrients if item['nutrient']['id'] == sugar_field), None)
     nutrient_level = NutrientLevel(
         fat=fat_level,
-        salt= salt_level,
+        salt=salt_level,
         saturated_fats=saturated_fats_level,
         sugar=sugar_level,
     )
