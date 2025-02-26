c9f0f3b8ba0d8e7b8f5e618343a9fde3ca0905ad
Estrella <estrella.paoli1@gmail.com>
2025-02-26 12:22:59 -0500

    corrected off csv fields types analysis

--- scripts/analysis/datasets_analyzer.py
+++ scripts/analysis/datasets_analyzer.py
@@ -1,11 +1,11 @@
 import logging
 import csv
 import json
+from datetime import datetime
 import ijson
-import sys
 
 
-def analyze_off_csv_data(filename: str, limit: int = None) -> dict:
+def analyze_off_csv_data(filename: str, nonetype_included: bool, limit: int = None) -> (dict, set):
     if limit is not None:
         logging.info(f"Analyzing the data of {limit} products from Open Food Facts csv dataset...")
     else:
@@ -20,16 +20,27 @@ def analyze_off_csv_data(filename: str, limit: int = None) -> dict:
         reader = csv.reader(f, delimiter="\t")
         header = next(reader)
 
-        fields_types = dict.fromkeys(header, {})
+        fields_types = {}
+        fields_can_be_none = set()
 
         for row in reader:
             for field_index in range(len(header)):
                 field, field_value = header[field_index], row[field_index]
 
-                if not type(field_value).__name__ in fields_types[field].keys():
-                    fields_types[field] = {type(field_value).__name__: 1}
-                else:
-                    fields_types[field][type(field_value).__name__] += 1
+                field_type = get_field_type(field, field_value)
+
+                if nonetype_included or field_type != type(None).__name__:
+                    if field not in fields_types.keys():
+                        fields_types[field] = {field_type: 1}
+                    elif field_type not in fields_types[field].keys():
+                        fields_types[field][field_type] = 1
+                    else:
+                        fields_types[field][field_type] += 1
+                elif field not in fields_types.keys():
+                    fields_types[field] = {}
+
+                if field_type == type(None).__name__:
+                    fields_can_be_none.add(field)
 
             n += 1
 
@@ -37,12 +48,65 @@ def analyze_off_csv_data(filename: str, limit: int = None) -> dict:
                 break
 
     logging.info("OFF data analyzed.")
-    result = ""
-    for field in fields_types.keys():
-        result += f"\n\t{field}: {fields_types[field]}"
-    logging.info(result)
+    show_fields_report(fields_types, fields_can_be_none)
 
-    return fields_types
+    return fields_types, fields_can_be_none
+
+
+def get_field_type(field: str, field_value: str) -> str:
+    field_type = get_particular_cases_type(field, field_value)
+
+    if not field_type:
+        field_type = type(None).__name__
+
+        if value_is_int(field_value):
+            field_type = int.__name__
+        elif value_is_float(field_value):
+            field_type = float.__name__
+        elif value_is_list(field_value):
+            field_type = list.__name__
+        elif value_is_string(field_value):
+            field_type = str.__name__
+
+    return field_type
+
+
+def get_particular_cases_type(field: str, field_value: str) -> str:
+    if field_value == "":
+        return type(None).__name__
+    if field == "code" or "_name" in field:
+        return str.__name__
+    if "_tags" in field:
+        return list.__name__
+    if "_100g" in field:
+        return float.__name__
+    if "_t" in field[:-2]:
+        return int.__name__
+    if "_datetime" in field:
+        return datetime.__name__
+    return None
+
+
+def value_is_float(field_value: str) -> bool:
+    is_float = False
+    try:
+        float(field_value)
+        is_float = True
+    except ValueError:
+        pass
+    return is_float
+
+
+def value_is_int(field_value: str) -> bool:
+    return field_value.isdigit()
+
+
+def value_is_list(field_value: str) -> bool:
+    return ',' in field_value
+
+
+def value_is_string(field_value: str) -> bool:
+    return field_value != ''
 
 
 def analyze_off_jsonl_data(filename: str, nonetype_included: bool, limit: int = None) -> (dict, set):
@@ -71,7 +135,7 @@ def analyze_off_jsonl_data(filename: str, nonetype_included: bool, limit: int = 
                 logging.info(f"Error parsing line: {line}. Error: {e}")
 
     logging.info("OFF data analyzed.")
-    show_json_report(fields_types, fields_can_be_none)
+    show_fields_report(fields_types, fields_can_be_none)
 
     return fields_types, fields_can_be_none
 
@@ -97,7 +161,7 @@ def analyze_fdc_data(filename: str, nonetype_included: bool, limit: int = None) 
                 break
 
     logging.info("FDC data analyzed.")
-    show_json_report(fields_types, fields_can_be_none)
+    show_fields_report(fields_types, fields_can_be_none)
 
     return fields_types, fields_can_be_none
 
@@ -120,7 +184,7 @@ def analyze_obj(obj: dict, fields_types: dict, fields_can_be_none: set, nonetype
     return fields_types, fields_can_be_none
 
 
-def show_json_report(fields_types: dict, fields_can_be_none: set) -> None:
+def show_fields_report(fields_types: dict, fields_can_be_none: set) -> None:
     types = ""
     for field in fields_types:
         types += f"\n\t{field}: {fields_types[field]}"
@@ -184,14 +248,18 @@ if __name__ == "__main__":
         handlers=[logging.StreamHandler()],
     )
 
-    off_csv_field_types = analyze_off_csv_data(r"C:\Users\estre\Documents\stage\bds\off\full\en.openfoodfacts.org.products.csv\en.openfoodfacts.org.products.csv", 2000)
+    off_csv_field_types, off_csv_none = analyze_off_csv_data(
+        r"C:\Users\estre\Documents\stage\bds\off\full\en.openfoodfacts.org.products.csv\en.openfoodfacts.org.products.csv", False, 1000)
     off_jsonl_field_types, off_jsonl_none = analyze_off_jsonl_data(r"C:\Users\estre\Documents\stage\bds\off\full\filtered_canada_products.json", False, 1000)
-    fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json", False, 1000)
+    fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json",
+                                                 False, 1000)
 
-    fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json", False, 1000)
+    fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json",
+                                                 False, 1000)
 
     show_inconsistent_fields(off_csv_field_types)
     show_inconsistent_fields(off_jsonl_field_types)
     show_inconsistent_fields(fdc_field_types)
 
-    record_values_for_field("countries", True, r"C:\Users\estre\Documents\stage\bds\off\full\en.openfoodfacts.org.products.csv\en.openfoodfacts.org.products.csv")
+    record_values_for_field("nutriscore_grade", True,
+                            r"C:\Users\estre\Documents\stage\bds\off\full\en.openfoodfacts.org.products.csv\en.openfoodfacts.org.products.csv")
