5b2a426f5a2974d7459c7ca86664224793ed01c3
Estrella <estrella.paoli1@gmail.com>
2025-02-26 10:47:05 -0500

    added record_values_for_field function

--- scripts/analysis/datasets_analyzer.py
+++ scripts/analysis/datasets_analyzer.py
@@ -2,6 +2,7 @@ import logging
 import csv
 import json
 import ijson
+import sys
 
 
 def analyze_off_csv_data(filename: str, limit: int = None) -> dict:
@@ -134,11 +135,46 @@ def show_json_report(fields_types: dict, fields_can_be_none: set) -> None:
 def show_inconsistent_fields(field_types: dict) -> None:
     inconsistent_fields = {}
     for field in field_types.keys():
-        if len(field_types[field].keys()) > 1:
+        if len([key for key in field_types[field].keys() if key != 'NoneType']) > 1:
             inconsistent_fields[field] = list(field_types[field].keys())
 
     logging.info(f"Inconsistent fields: {inconsistent_fields}")
 
+
+def record_values_for_field(field_name: str, show_values: bool, filename: str, limit: int = None) -> set:
+    if limit is not None:
+        logging.info(f"Recording {limit} values for {field_name} field from Open Food Facts csv dataset...")
+    else:
+        logging.info(f"Recording all values for {field_name} field from Open Food Facts csv dataset...")
+
+    # Increase the CSV field size limit to avoid the error:
+    # _csv.Error: field larger than field limit (131072)
+    csv.field_size_limit(2 ** 30)
+
+    n = 0
+    with open(filename, "r", encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter="\t")
+        header = next(reader)
+        values = set()
+
+        for row in reader:
+            idx = header.index(field_name)
+            if row[idx] != '':
+                values.add(row[idx])
+
+            n += 1
+
+            if limit is not None and n >= limit:
+                break
+
+    if show_values:
+        values_list = ""
+        for value in values:
+            values_list += f"\n\t{value}"
+        logging.info(f"Values found:{values_list}")
+
+    logging.info(f"values for {field_name} field recorded")
+    return values
 
 
 if __name__ == "__main__":
@@ -152,6 +188,10 @@ if __name__ == "__main__":
     off_jsonl_field_types, off_jsonl_none = analyze_off_jsonl_data(r"C:\Users\estre\Documents\stage\bds\off\full\filtered_canada_products.json", False, 1000)
     fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json", False, 1000)
 
+    fdc_field_types, fdc_none = analyze_fdc_data(r"C:\Users\estre\Documents\stage\bds\fdc\FoodData_Central_branded_food_json_2024-10-31\brandedDownload.json", False, 1000)
+
     show_inconsistent_fields(off_csv_field_types)
     show_inconsistent_fields(off_jsonl_field_types)
     show_inconsistent_fields(fdc_field_types)
+
+    record_values_for_field("countries", True, r"C:\Users\estre\Documents\stage\bds\off\full\en.openfoodfacts.org.products.csv\en.openfoodfacts.org.products.csv")
