64eef6b31b7b578a9e47bedea74908b9de78340b
Estrella <estrella.paoli1@gmail.com>
2025-02-28 11:23:16 -0500

    added fdc fields documentation and adapted code to use it

--- scripts/analysis/fields_informations_describer.py
+++ scripts/analysis/fields_informations_describer.py
@@ -3,50 +3,79 @@ import ijson
 import json
 
 
-def show_all_fields():
+def show_all_off_csv_fields(dataset: str):
     """Shows the names of all the fields in the documentation"""
-    filename = r"../../off_csv_fields_descriptions.json"
+    files = {'OFF': r"../../off_csv_fields_descriptions.json", 'FDC': r"../../fdc_fields_descriptions.json"}
 
-    fields_list = "Fields of the csv file for Open Food Facts:"
-    with open(filename, "r", encoding="utf-8") as file:
-        for obj in ijson.items(file, "fields.item"):
-            fields_list += f"\n\t{obj['name']}"
-    logging.info(fields_list)
+    try:
+        filename = files[dataset]
+
+        fields_list = "Fields of the csv file for Open Food Facts:"
+        with open(filename, "r", encoding="utf-8") as file:
+            for obj in ijson.items(file, "fields.item"):
+                fields_list += f"\n\t{obj['name']}"
+        logging.info(fields_list)
+
+    except KeyError:
+        logging.info(f"The dataset {dataset} is not available. Available datasets: {list(files.keys())}")
+        return
 
 
-def show_field_description(field_name):
+def show_field_description(dataset: str, field_name: str):
     """Shows the description of a given field name in the documentation"""
-    filename = r"../../off_csv_fields_descriptions.json"
+    files = {'OFF': r"../../off_csv_fields_descriptions.json", 'FDC': r"../../fdc_fields_descriptions.json"}
 
-    field_description = f"Information about {field_name}:"
-    with open(filename, "r", encoding="utf-8") as file:
-        data = json.load(file)
-        fields = data.get("fields", [])
+    try:
+        filename = files[dataset]
 
-        field = next(
-            (document for document in fields if document.get("name") == field_name),
-            None,
-        )
+        field_description = f"Information about {field_name}:"
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            fields = data.get("fields", [])
 
-        if field:
-            for item in field.keys():
-                field_description += f"\n\t{item}: {field[item]}"
-        else:
-            field_description += (
-                f"\n\tnot found: {field_name} is not a field in the CSV file"
+            field = next(
+                (document for document in fields if document.get("name") == field_name),
+                None,
             )
 
-        logging.info(field_description)
+            if field:
+                for item in field.keys():
+                    field_description += f"\n\t{item}: {field[item]}"
+            else:
+                field_description += (
+                    f"\n\tnot found: {field_name} is not a field in the CSV file"
+                )
+
+            logging.info(field_description)
+
+    except KeyError:
+        logging.info(f"The dataset {dataset} is not available. Available datasets: {list(files.keys())}")
+        return
 
 
-def show_all_fields_descriptions():
-    """Shows the descriptions of all the fields in the documentation"""
-    filename = r"../../off_csv_fields_descriptions.json"
+def show_all_fields_descriptions(dataset: str):
+    """Shows the descriptions of all the fields in the chosen documentation"""
+    files = {'OFF': r"../../off_csv_fields_descriptions.json", 'FDC': r"../../fdc_fields_descriptions.json"}
 
-    fields_descriptions = ""
-    with open(filename, "r", encoding="utf-8") as file:
-        for field in ijson.items(file, "fields.item"):
-            fields_descriptions += f"\nInformation about {field['name']}:"
-            for item in field.keys():
-                fields_descriptions += f"\n\t{item}: {field[item]}"
-    logging.info(fields_descriptions)
+    try:
+        filename = files[dataset]
+
+        fields_descriptions = ""
+        with open(filename, "r", encoding="utf-8") as file:
+            for field in ijson.items(file, "fields.item"):
+                fields_descriptions += f"\nInformation about {field['name']}:"
+                for item in field.keys():
+                    fields_descriptions += f"\n\t{item}: {field[item]}"
+        logging.info(fields_descriptions)
+
+    except KeyError:
+        logging.info(f"The dataset {dataset} is not available. Available datasets: {list(files.keys())}")
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+show_field_description("FDC", "foodAttributes")
