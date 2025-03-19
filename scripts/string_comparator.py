2081a2388d9a99c46f65dbc8037409433032c711
Zack Dufresne <zack.dufresne.1@ulaval.ca>
2025-03-19 00:30:33 -0400

    added unit tests

--- scripts/string_comparator.py
+++ scripts/string_comparator.py
@@ -9,23 +9,23 @@ class StringComparator:
         return field == second_field
 
     @staticmethod
-    def is_identical_case_insensitive(self, field: str, second_field: str) -> bool:
+    def is_identical_case_insensitive(field: str, second_field: str) -> bool:
         lower_field = field.lower()
         second_lower_field = second_field.lower()
 
-        return self.is_identical(lower_field, second_lower_field)
+        return StringComparator.is_identical(lower_field, second_lower_field)
 
     @staticmethod
-    def is_identical_case_white_space(self, field: str, second_field: str) -> bool:
+    def is_identical_case_white_space(field: str, second_field: str) -> bool:
         white_space_field = re.sub(r"\s+", "", field)
         white_space_second_field = re.sub(r"\s+", "", second_field)
 
-        return self.is_identical_case_insensitive(
+        return StringComparator.is_identical_case_insensitive(
             white_space_field, white_space_second_field
         )
 
     @staticmethod
-    def compare_string(self, field: str, second_field: str) -> bool:
+    def compare_string(field: str, second_field: str) -> bool:
         normalized_field = "".join(
             c
             for c in unicodedata.normalize("NFD", field)
@@ -37,7 +37,7 @@ class StringComparator:
             if unicodedata.category(c) != "Mn"
         )
 
-        return self.is_identical_case_white_space(
+        return StringComparator.is_identical_case_white_space(
             normalized_field, normalized_second_field
         )
 
@@ -47,15 +47,15 @@ class StringComparator:
 
     @staticmethod
     def is_lists_have_same_elements(
-        self, first_list: List[str], second_list: List[str]
+        first_list: List[str], second_list: List[str]
     ) -> bool:
         for i in range(len(first_list)):
-            if not self.compare_string(first_list[i], second_list[i]):
+            if not StringComparator.compare_string(first_list[i], second_list[i]):
                 return False
         return True
 
     @staticmethod
-    def compare_list(self, first_list: List[str], second_list: List[str]):
-        return self.is_list_same_length(
+    def compare_list(first_list: List[str], second_list: List[str]):
+        return StringComparator.is_list_same_length(
             first_list, second_list
-        ) and self.is_lists_have_same_elements(first_list, second_list)
+        ) and StringComparator.is_lists_have_same_elements(first_list, second_list)
