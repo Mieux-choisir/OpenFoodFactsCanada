dc503e8c4fecf071887a0666e3261c5fc6847241
nichalim5 <nihal5@ulaval.ca>
2025-04-05 17:41:22 -0400

    fix tests

--- tests/scripts/test_number_comparator.py
+++ tests/scripts/test_number_comparator.py
@@ -24,39 +24,3 @@ def test_should_return_true_when_is_the_same_number(number_comparator):
 
 def test_should_return_false_when_number_is_different(number_comparator):
     assert number_comparator.is_same_number(NUMBER, DIFFERENT_NUMBER) is False
-
-
-def test_should_return_true_when_ratio_are_equal(number_comparator):
-    assert (
-        number_comparator.check_value_per_100g(
-            RATIO_NUMERATOR,
-            RATIO_DENOMINATOR,
-            SAME_RATIO_NUMERATOR,
-            SAME_RATIO_DENOMINATOR,
-        )
-        is True
-    )
-
-
-def test_should_return_false_when_ratio_are_not_equal(number_comparator):
-    assert (
-        number_comparator.check_value_per_100g(
-            RATIO_NUMERATOR,
-            RATIO_DENOMINATOR,
-            OTHER_RATIO_NUMERATOR,
-            OTHER_RATIO_DENOMINATOR,
-        )
-        is False
-    )
-
-
-def test_should_raise_error_when_dividing_by_zero(number_comparator):
-    with pytest.raises(ZeroDivisionError):
-        number_comparator.check_value_per_100g(
-            RATIO_NUMERATOR, 0, RATIO_NUMERATOR, RATIO_DENOMINATOR
-        )
-
-    with pytest.raises(ZeroDivisionError):
-        number_comparator.check_value_per_100g(
-            RATIO_NUMERATOR, RATIO_DENOMINATOR, RATIO_NUMERATOR, 0
-        )
