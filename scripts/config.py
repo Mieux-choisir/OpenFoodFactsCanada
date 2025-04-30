34bdb787587cd7fc14caf75431c48dc99698c1e1
David Dionne-Pelletier <david.dionne-pelletier.1@ulaval.ca>
2025-04-30 13:42:39 -0400

    change fdc json url

--- scripts/config.py
+++ scripts/config.py
@@ -28,7 +28,7 @@ class Config:
             "https://static.openfoodfacts.org/data/openfoodfacts-products.jsonl.gz"
         )
         self.fdc_json_url: str = (
-            "https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_branded_food_json_2024-10-31.zip"
+            "https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_branded_food_json_2025-04-24.zip"
         )
 
         self.off_compressed_csv_file_name: str = "off_csv.gz"
