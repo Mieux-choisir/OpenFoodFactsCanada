60dfaf5029b9d86b68a1150197216b9dc5c64e9c
Estrella <estrella.paoli1@gmail.com>
2025-06-05 11:08:53 -0400

    documented StatisticsCreator class

--- scripts/statistics_creator.py
+++ scripts/statistics_creator.py
@@ -6,6 +6,18 @@ from pymongo import MongoClient
 
 
 class StatisticsCreator:
+    """
+    This is a class that creates statistics on products.
+
+    Methods:
+        make_merge_summary(overwrite_counter, complete_counter, count_skipped, count_off_products, count_fdc_products): Provides hte merge summary
+        compare_number_differences(threshold, use_docker): For each numerical field, provides the number of products that have been completed, updated below the
+        given threshold, updated above the given threshold and that have the exact same value.
+        create_csv_modification_report(use_docker): Creates a csv file that shows the names of the modified fields and their OFF and FDC values on each line.
+        create_csv_modification_report_for_field(field_name, analyze_empty_fields, use_docker): Creates a csv file that shows the names of the OFF and FDC
+        values of a given field when they are different on each line for a given number of products.
+    """
+
     def make_merge_summary(
         self,
         overwrite_counter,
@@ -14,6 +26,9 @@ class StatisticsCreator:
         count_off_products,
         count_fdc_products,
     ):
+        """Provides the merge summary.
+        Provides the number of products that have been completed and overwritten for each field, and its corresponding percentage
+        """
         logging.info("=== Merge Summary ===")
 
         compute_percentages = True
@@ -35,8 +50,10 @@ class StatisticsCreator:
         )
 
     def compare_number_differences(self, threshold=0.1, use_docker=True):
+        """For each numerical field, provides the number of products that have been completed, updated below the given threshold,
+        updated above the given threshold and that have the exact same value."""
         print("Starting statistics comparison ...")
-        _, off_cursor, _, final_cursor = self.__get_products(use_docker)
+        off_cursor, _, final_cursor = self.__get_products(use_docker)
 
         off_product = next(off_cursor, None)
         final_product = next(final_cursor, None)
@@ -117,112 +134,12 @@ class StatisticsCreator:
 
         return fields_differences_count
 
-    def __count_for_field(
-        self, fields_differences_count, field, off_value, final_value, threshold
-    ):
-        if off_value is None and final_value is not None:
-            fields_differences_count = self.__update_absent_counts(
-                fields_differences_count, field
-            )
-        elif (
-            off_value is not None
-            and final_value is not None
-            and abs(off_value - final_value) >= threshold
-        ):
-            fields_differences_count = self.__update_modified_above_threshold_counts(
-                fields_differences_count, field
-            )
-        elif (
-            off_value is not None
-            and final_value is not None
-            and abs(off_value - final_value) > 0
-        ):
-            fields_differences_count = self.__update_modified_below_threshold_counts(
-                fields_differences_count, field
-            )
-        else:
-            fields_differences_count = self.__update_same_value_counts(
-                fields_differences_count, field
-            )
-        return fields_differences_count
+    def create_csv_modification_report(self, limit=500, use_docker=True):
+        """Creates a csv file that shows the names of the modified fields and their OFF and FDC values on each line for a given number of products."""
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_dir = os.path.dirname(script_dir)
 
-    @staticmethod
-    def __update_absent_counts(fields_differences_count, field):
-        if field not in fields_differences_count.keys():
-            fields_differences_count[field] = {
-                "completed": 0,
-                "modified_above_threshold": 0,
-                "modified_below_threshold": 0,
-                "same_value": 0,
-            }
-        fields_differences_count.get(field)["completed"] += 1
-        return fields_differences_count
-
-    @staticmethod
-    def __update_modified_above_threshold_counts(fields_differences_count, field):
-        if field not in fields_differences_count.keys():
-            fields_differences_count[field] = {
-                "completed": 0,
-                "modified_above_threshold": 0,
-                "modified_below_threshold": 0,
-                "same_value": 0,
-            }
-        fields_differences_count.get(field)["modified_above_threshold"] += 1
-        return fields_differences_count
-
-    @staticmethod
-    def __update_modified_below_threshold_counts(fields_differences_count, field):
-        if field not in fields_differences_count.keys():
-            fields_differences_count[field] = {
-                "completed": 0,
-                "modified_above_threshold": 0,
-                "modified_below_threshold": 0,
-                "same_value": 0,
-            }
-        fields_differences_count.get(field)["modified_below_threshold"] += 1
-        return fields_differences_count
-
-    @staticmethod
-    def __update_same_value_counts(fields_differences_count, field):
-        if field not in fields_differences_count.keys():
-            fields_differences_count[field] = {
-                "completed": 0,
-                "modified_above_threshold": 0,
-                "modified_below_threshold": 0,
-                "same_value": 0,
-            }
-        fields_differences_count.get(field)["same_value"] += 1
-        return fields_differences_count
-
-    @staticmethod
-    def __make_overwritten_summary(overwrite_counter, count_total, compute_percentages):
-        logging.info("Most overwritten fields:")
-        for field, count in overwrite_counter.most_common():
-            message = f"  {field}: {count} times"
-            if compute_percentages:
-                message += f" [{round(count * 100 / count_total, 2)}%]"
-            logging.info(message)
-
-    @staticmethod
-    def __make_completed_summary(complete_counter, count_total, compute_percentages):
-        logging.info("Most completed fields:")
-        for field, count in complete_counter.most_common():
-            message = f"  {field}: {count} times"
-            if compute_percentages:
-                message += f" [{round(count * 100 / count_total, 2)}%]"
-            logging.info(message)
-
-    @staticmethod
-    def __make_skipped_summary(count_skipped, count_total, compute_percentages):
-        message = f"Total skipped products: {count_skipped}"
-        if compute_percentages:
-            message += f" [{round(count_skipped * 100 / count_total, 2)}%]"
-        logging.info(message)
-
-    def create_csv_modification_report(self, use_docker=True):
-        parent_dir, off_cursor, fdc_cursor, final_cursor = self.__get_products(
-            use_docker
-        )
+        off_cursor, fdc_cursor, final_cursor = self.__get_products(use_docker)
 
         off_product = next(off_cursor, None)
         fdc_product = next(fdc_cursor, None)
@@ -247,55 +164,21 @@ class StatisticsCreator:
                     fdc_product,
                     final_product,
                 )
-                if nb_products >= 500:
+                if nb_products >= limit:
                     return
                 off_product = next(off_cursor, None)
                 fdc_product = next(fdc_cursor, None)
                 final_product = next(final_cursor, None)
             logging.info("Csv file created!")
 
-    def __write_modified_values(
-        self, filewriter, product_id, off, fdc, final, parent_key=""
-    ):
-        if (
-            not isinstance(off, dict)
-            or not isinstance(fdc, dict)
-            or not isinstance(final, dict)
-        ):
-            pass
-
-        for key in set(off.keys()).union(fdc.keys()).union(final.keys()):
-            full_key = f"{parent_key}.{key}" if parent_key else key
-            off_value = off.get(key)
-            fdc_value = fdc.get(key)
-            final_value = final.get(key)
-
-            if (
-                isinstance(off_value, dict)
-                and isinstance(fdc_value, dict)
-                and isinstance(final_value, dict)
-            ):
-                self.__write_modified_values(
-                    filewriter, product_id, off_value, fdc_value, final_value, full_key
-                )
-
-            elif off_value != final_value and full_key not in [
-                "_id",
-                "fdc_id",
-                "modified_date",
-                "data_source",
-                "id_original",
-                "fdc_category_en",
-            ]:
-                line = [product_id, full_key, off_value, final_value, fdc_value]
-                filewriter.writerow(line)
-
     def create_csv_modification_report_for_field(
-        self, field_name, analyze_empty_fields=True, use_docker=True
+        self, field_name, limit=2000, analyze_empty_fields=True, use_docker=True
     ):
-        parent_dir, off_cursor, fdc_cursor, final_cursor = self.__get_products(
-            use_docker
-        )
+        """Creates a csv that shows the names of the OFF and FDC values of a given field when they are different on each line for a given number of products."""
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_dir = os.path.dirname(script_dir)
+
+        off_cursor, fdc_cursor, final_cursor = self.__get_products(use_docker)
 
         off_product = next(off_cursor, None)
         fdc_product = next(fdc_cursor, None)
@@ -336,7 +219,7 @@ class StatisticsCreator:
                     filewriter.writerow(line)
                     nb_products += 1
 
-                if nb_products >= 2000:
+                if nb_products >= limit:
                     break
 
                 off_product = next(off_cursor, None)
@@ -346,10 +229,158 @@ class StatisticsCreator:
             logging.info("Csv file created!")
 
     @staticmethod
-    def __get_products(use_docker=True):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        parent_dir = os.path.dirname(script_dir)
+    def __make_overwritten_summary(overwrite_counter, count_total, compute_percentages):
+        """Provides the summary of overwritten fields for updated products."""
+        logging.info("Most overwritten fields:")
+        for field, count in overwrite_counter.most_common():
+            message = f"  {field}: {count} times"
+            if compute_percentages:
+                message += f" [{round(count * 100 / count_total, 2)}%]"
+            logging.info(message)
 
+    @staticmethod
+    def __make_completed_summary(complete_counter, count_total, compute_percentages):
+        """Provides the summary of completed fields for updated products."""
+        logging.info("Most completed fields:")
+        for field, count in complete_counter.most_common():
+            message = f"  {field}: {count} times"
+            if compute_percentages:
+                message += f" [{round(count * 100 / count_total, 2)}%]"
+            logging.info(message)
+
+    @staticmethod
+    def __make_skipped_summary(count_skipped, count_total, compute_percentages):
+        """Provides the number of skipped products during the merge."""
+        message = f"Total skipped products: {count_skipped}"
+        if compute_percentages:
+            message += f" [{round(count_skipped * 100 / count_total, 2)}%]"
+        logging.info(message)
+
+    def __count_for_field(
+        self, fields_differences_count, field, off_value, final_value, threshold
+    ):
+        """Counts the number of times a given field is completed, updated below the given threshold,
+        updated above the given threshold and with the exact same value between two given values.
+        """
+        if off_value is None and final_value is not None:
+            fields_differences_count = self.__update_absent_counts(
+                fields_differences_count, field
+            )
+        elif (
+            off_value is not None
+            and final_value is not None
+            and abs(off_value - final_value) >= threshold
+        ):
+            fields_differences_count = self.__update_modified_above_threshold_counts(
+                fields_differences_count, field
+            )
+        elif (
+            off_value is not None
+            and final_value is not None
+            and abs(off_value - final_value) > 0
+        ):
+            fields_differences_count = self.__update_modified_below_threshold_counts(
+                fields_differences_count, field
+            )
+        else:
+            fields_differences_count = self.__update_same_value_counts(
+                fields_differences_count, field
+            )
+        return fields_differences_count
+
+    @staticmethod
+    def __update_absent_counts(fields_differences_count, field):
+        """Increments the number of times a given field is completed."""
+        if field not in fields_differences_count.keys():
+            fields_differences_count[field] = {
+                "completed": 0,
+                "modified_above_threshold": 0,
+                "modified_below_threshold": 0,
+                "same_value": 0,
+            }
+        fields_differences_count.get(field)["completed"] += 1
+        return fields_differences_count
+
+    @staticmethod
+    def __update_modified_above_threshold_counts(fields_differences_count, field):
+        """Increments the number of times a given field is updated above a threshold."""
+        if field not in fields_differences_count.keys():
+            fields_differences_count[field] = {
+                "completed": 0,
+                "modified_above_threshold": 0,
+                "modified_below_threshold": 0,
+                "same_value": 0,
+            }
+        fields_differences_count.get(field)["modified_above_threshold"] += 1
+        return fields_differences_count
+
+    @staticmethod
+    def __update_modified_below_threshold_counts(fields_differences_count, field):
+        """Increments the number of times a given field is updated below a threshold."""
+        if field not in fields_differences_count.keys():
+            fields_differences_count[field] = {
+                "completed": 0,
+                "modified_above_threshold": 0,
+                "modified_below_threshold": 0,
+                "same_value": 0,
+            }
+        fields_differences_count.get(field)["modified_below_threshold"] += 1
+        return fields_differences_count
+
+    @staticmethod
+    def __update_same_value_counts(fields_differences_count, field):
+        """Increments the number of times a given field is with the exact same value."""
+        if field not in fields_differences_count.keys():
+            fields_differences_count[field] = {
+                "completed": 0,
+                "modified_above_threshold": 0,
+                "modified_below_threshold": 0,
+                "same_value": 0,
+            }
+        fields_differences_count.get(field)["same_value"] += 1
+        return fields_differences_count
+
+    def __write_modified_values(
+        self, filewriter, product_id, off, fdc, final, parent_key=""
+    ):
+        """Writes the OFF and FDC values of a given field along with the id of the corresponding product."""
+        if (
+            not isinstance(off, dict)
+            or not isinstance(fdc, dict)
+            or not isinstance(final, dict)
+        ):
+            pass
+
+        for key in set(off.keys()).union(fdc.keys()).union(final.keys()):
+            full_key = f"{parent_key}.{key}" if parent_key else key
+            off_value = off.get(key)
+            fdc_value = fdc.get(key)
+            final_value = final.get(key)
+
+            if (
+                isinstance(off_value, dict)
+                and isinstance(fdc_value, dict)
+                and isinstance(final_value, dict)
+            ):
+                self.__write_modified_values(
+                    filewriter, product_id, off_value, fdc_value, final_value, full_key
+                )
+
+            elif off_value != final_value and full_key not in [
+                "_id",
+                "fdc_id",
+                "modified_date",
+                "data_source",
+                "id_original",
+                "fdc_category_en",
+            ]:
+                line = [product_id, full_key, off_value, final_value, fdc_value]
+                filewriter.writerow(line)
+
+    @staticmethod
+    def __get_products(use_docker=True):
+        """Retrieves the products of the matched OFF products, the matched FDC products and the final products created by the merge.
+        Returns the cursors of the OFF, the FDC and the final products."""
         connection_string = (
             "mongodb://mongo:27017/" if use_docker else "mongodb://localhost:37017"
         )
@@ -365,4 +396,4 @@ class StatisticsCreator:
         fdc_cursor = iter(matched_fdc_products)
         final_cursor = iter(final_products)
 
-        return parent_dir, off_cursor, fdc_cursor, final_cursor
+        return off_cursor, fdc_cursor, final_cursor
